0fa8f2fcdbdd565efd9760e77cc84a8172660f01
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-06-10 17:22:18 +0530

    Add files via upload

---
+++ app124.js
@@ -0,0 +1,25 @@
+h1 = document.querySelector("h1");
+
+function changeColor(color,delay, nextColorChange) {
+    setTimeout(() => {
+    h1.style.color = color; 
+    if(nextColorChange) nextColorChange();
+}, delay);
+}
+
+
+changeColor("red" , 1000 , () => {
+  changeColor("orange" , 1000, () => {
+    changeColor("green" , 1000, () => {
+        changeColor("yellow", 1000, () => {
+            changeColor("blue", 1000);
+        });
+    });
+  });
+});
+
+//callbacks nesting -> callback hell
+
+
+
+// changeColor("green" , 3000);
